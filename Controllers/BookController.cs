321b65ea2ce24dd6a15cf0da6cc72beea6ee8069
gugu <Exaze Trsa25@EXZ_MP2HMR98>
2025-05-29 10:03:46 +0200

    Inital Commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        
+        
+    }
+}
