83d904447efabeae425235b970ac95859fb70f6c
gugu <Exaze Trsa25@EXZ_MP2HMR98>
2025-05-30 16:01:20 +0200

    1.modified dbcontext, by adding Dbsets 2.  added Jwt class  that generates Jwt tokenfor authenticated users 3.added connection string  and Add configuration for jwt setup 4. updated nugpackages 5. Added authentication on program.cs ,JWT config

--- Data/LibraryDbContext.cs
+++ Data/LibraryDbContext.cs
@@ -1,8 +1,24 @@
-﻿using LibraryAPI.Models;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿
+using LibraryAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace LibraryAPI.Data
 {
-   
+    public class LibraryDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) {}
+        
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Fine> Fines { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
+
+    }
+
 }
---
+++ Services/JwtService.cs
@@ -0,0 +1,44 @@
+﻿using LibraryAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LibraryAPI.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(ApplicationUser user, IList<string> roles)
+        {
+            var authClaims = new List<Claim>
+
+            {
+            new Claim(ClaimTypes.Name,user.FullName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier,user.Id)
+            };
+
+            foreach (var role in roles) 
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpiresInMinutes"])),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
