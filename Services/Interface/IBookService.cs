c33414f4bc595fbc4354bb745d4fbc66494ac82f
gugu <Exaze Trsa25@EXZ_MP2HMR98>
2025-06-04 09:19:37 +0200

    Implemented repository , service , controller end point for Book entity

---
+++ Services/Interface/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryAPI.DTOs.BookDTOs;
+
+namespace LibraryAPI.Services.Interface
+{
+    public interface IBookService
+    {
+        Task<List<BookViewDto>> GetAllBooksAsync();
+        Task<BookViewDto?> GetBookByIdAsync(int id);
+        Task AddBookAsync(BookViewDto bookViewDto);
+        Task<bool> DeleteBookAsync(int id);
+
+    }
+}
